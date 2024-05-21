1c2991a5d35a09f8995186096656a21726eecd5b
riad-challal <lr_challal@esi.dz>
2024-05-21 03:54:57 +0100

    anamneseModel implementation

--- src/main/java/Models/Anamnese/AnamneseSchema.java
+++ src/main/java/Models/Anamnese/AnamneseSchema.java
@@ -5,13 +5,35 @@ import java.util.ArrayList;
 import Exceptions.ElementNotFoundException;
 
 public class AnamneseSchema implements Serializable {
+    private String titre;
     private ArrayList<AnamneseQuestion> questionsAnamnese;
 
-    public AnamneseSchema(ArrayList<AnamneseQuestion> questionsAnamnese) {
+    public AnamneseSchema(String titre, ArrayList<AnamneseQuestion> questionsAnamnese) {
+        this.titre = titre;
         this.questionsAnamnese = questionsAnamnese;
     }
 
+    public AnamneseSchema(String titre) {
+        this.titre = titre;
+    }
+
     public AnamneseSchema() {
+    }
+
+    public String getTitre() {
+        return titre;
+    }
+
+    public void setTitre(String titre) {
+        this.titre = titre;
+    }
+
+    public ArrayList<AnamneseQuestion> getQuestionsAnamnese() {
+        return questionsAnamnese;
+    }
+
+    public void setQuestionsAnamnese(ArrayList<AnamneseQuestion> questionsAnamnese) {
+        this.questionsAnamnese = questionsAnamnese;
     }
 
     public void addQuestion(AnamneseQuestion AQ) {
